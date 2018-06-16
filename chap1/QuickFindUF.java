632477d1ee068fd7d8b5f7c7ce25ead9e54c23da
izff <zhuustc@outlook.com>
2018-06-16 01:15:31 -0400

    tiny modify on code styles

--- chap1/QuickFindUF.java
+++ chap1/QuickFindUF.java
@@ -1,3 +1,4 @@
+//package edu.princeton.cs.algs4;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -5,38 +6,36 @@ public class QuickFindUF {
     private int[] id;
     private int count;
 
-    public QuickFindUF(int n)
-    {
+    public QuickFindUF(int n) {
         count = n;
         id = new int[n];
-        for (int i=0; i<n; i++)
+        for (int i = 0; i < n; i++)
             id[i] = i;
     }
 
-    public int count(){
+    public int count() {
         return count;
     }
 
-    public int find(int p){
+    public int find(int p) {
         validate(p);
         return id[p];
     }
 
     private void validate(int p) {
-        int n=id.length;
-        if (p<0 || p>=n){
-            throw new IllegalArgumentException("Index " + p + " is not between 0 and " + (n-1));
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("Index " + p + " invalid!");
         }
     }
 
-    public boolean connected(int p, int q){
+    public boolean connected(int p, int q) {
         validate(p);
         validate(q);
         return id[p] == id[q];
     }
 
-    public void union(int p, int q)
-    {
+    public void union(int p, int q) {
         validate(p);
         validate(q);
         int pID = id[p];
@@ -44,19 +43,19 @@ public class QuickFindUF {
 
         if (pID == qID) return;
 
-        for (int i=0;i<id.length;i++)
-            if (id[i]==pID) id[i]=qID;
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
         count--;
     }
 
-    public static void main(String[] args){
-        int n=StdIn.readInt();
-        QuickFindUF uf= new QuickFindUF(n);
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(n);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             if (uf.connected(p, q)) continue;
-            uf.union(p,q);
+            uf.union(p, q);
             StdOut.println(p + " " + q);
         }
         StdOut.println(uf.count() + " components");
