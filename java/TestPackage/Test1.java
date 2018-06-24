8dceab950271b466ef0827d76a14b58c99b47db5
izff <zhuustc@outlook.com>
2018-06-24 16:32:02 -0400

    delete learnjava

--- java/TestPackage/Test1.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast;
-
-class Test1 {
-    public static void main(String[] args) {
-        com.mydemo.Demo d = new com.mydemo.Demo();
-        System.out.println(d.sum(10, 20));
-    }
-}
--- java/TestPackage/Test2.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast;
-import com.mydemo.Demo;
-
-class Test2 {
-    public static void main(String[] args) {
-        Demo d = new Demo();
-        System.out.println(d.sum(10, 20));
-    }
-}
