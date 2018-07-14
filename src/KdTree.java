8ba9e6536e8b66084911a8dd8de509a53d1c8f81
izff <zhuustc@outlook.com>
2018-07-14 16:57:41 -0400

    refraction

---
+++ src/KdTree.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+    private static final boolean HORIZONTAL = true;
+    private static final boolean VERTICAL = false;
+
+    public KdTree() {
+
+    }
+
+    public boolean isEmpty() {}
+
+    public void insert(Point2D p) {}
+
+    public boolean contains(Point2D p) {}
+
+    public Iterable<Point2D> range(RectHV rect) {}
+
+    public Point2D nearest(Point2D p) {
+    }
+
+    public static void main(String[] args) {
+        // skip testing now
+    }
+}
---
+++ src/PointSET.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import java.util.TreeSet;
+import java.util.ArrayList;
+
+public class PointSET {
+    private TreeSet<Point2D> plist;
+
+    public PointSET () {
+        plist = new TreeSet<>();
+    }
+
+    public boolean isEmpty() { return plist.isEmpty(); }
+
+    public int size() { return plist.size(); }
+
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("Null point!");
+        plist.add(p);
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("Null point!");
+        return plist.contains(p);
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException("Null rect!");
+
+        ArrayList<Point2D> points = new ArrayList<>();
+        for (Point2D p: plist) {
+            if (rect.contains(p))
+                points.add(p);
+        }
+        return points;
+    }
+
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("Null point!");
+
+        Point2D nearest = null;
+        double minDistance = Double.POSITIVE_INFINITY;
+        double temp;
+        for (Point2D pm: plist) {
+            temp = p.distanceTo(pm);
+            if (temp < minDistance) {
+                minDistance = temp;
+                nearest = pm;
+            }
+        }
+        return nearest;
+    }
+
+    public static void main(String[] args) {
+        // skip testing
+    }
+}
